8df73ecaf765c1ce767c059ac38e53209333c1fb
Ben Chappell <bjchappell02@icloud.com>
2021-03-14 16:52:54 -0400

    Added headers for driving files

---
+++ driving/between_walls.py
@@ -0,0 +1,30 @@
+# Project 3: GEARS Robot
+# File: between_walls.py
+# Date: 3/14/21
+# By: Alex Wiseman
+# ajwisema
+# Ben Chappell
+# chappeb
+# Trevor Moorman
+# tmoorma
+# Cole Kingery
+# ckinger
+# Section: 5
+# Team: 70
+#
+# ELECTRONIC SIGNATURE
+# Ben Chappell
+# Cole Kingery
+# Trevor Moorman
+# Alex Wiseman
+# 
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+# 
+# Keeps the robot from bumping into the walls while driving through corridors. Should implement basic control theory (as discussed in class).
+# Inputs: Wall Closeness, Driving State, Corridor Data
+# Outputs: Keeping bot between walls.
---
+++ driving/propulsion.py
@@ -0,0 +1,31 @@
+# Project 3: GEARS Robot
+# File: propulsion.py
+# Date: 3/14/21
+# By: Alex Wiseman
+# ajwisema
+# Ben Chappell
+# chappeb
+# Trevor Moorman
+# tmoorma
+# Cole Kingery
+# ckinger
+# Section: 5
+# Team: 70
+#
+# ELECTRONIC SIGNATURE
+# Ben Chappell
+# Cole Kingery
+# Trevor Moorman
+# Alex Wiseman
+# 
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+# 
+# Communicates with motor interfacing to engage the motors in potentially more complex maneuvers. 
+# Tells the motor interfacing what to do. Most driving code should at least go through here.
+# Inputs: Motor Data, Caller's Data
+# Outputs: Driving the Bot
