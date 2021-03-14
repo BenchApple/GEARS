1c4b01129bc5150b95e3f63821581e6118dabea8
Ben Chappell <bjchappell02@icloud.com>
2021-03-14 17:14:10 -0400

    added file headers

---
+++ walls/wall_sensing.py
@@ -0,0 +1,31 @@
+# Project 3: GEARS Robot
+# File: wall_sensing.py
+# Date: 3/14/21
+# By: Alex Wiseman
+# ajwisema
+# Ben Chappell
+# chappeb
+# Trevor Moorman
+# tmoorma
+# Cole Kingery
+# ckinger
+# Section: 5
+# Team: 70
+#
+# ELECTRONIC SIGNATURE
+# Ben Chappell
+# Cole Kingery
+# Trevor Moorman
+# Alex Wiseman
+# 
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+# 
+# Interfacing between ultrasonic data and various processing. Determines whether or not a corridor is passable or not and sends that to processing to turn into usable data.
+# Inputs: Ultrasonic Data, Hazard Existence
+# Outputs: Wall/Corridor Data
+
