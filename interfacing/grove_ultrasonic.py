6d00749d22ed39d9710e282f278ebf3dd2a13fe2
Ben Chappell <bjchappell02@icloud.com>
2021-03-14 17:05:19 -0400

    added file headers

---
+++ interfacing/grove_ultrasonic.py
@@ -0,0 +1,30 @@
+# Project 3: GEARS Robot
+# File: grove_ultrasonic.py
+# Date: 3/14/21
+# By: Alex Wiseman
+# ajwisema
+# Ben Chappell
+# chappeb
+# Trevor Moorman
+# tmoorma
+# Cole Kingery
+# ckinger
+# Section: 5
+# Team: 70
+#
+# ELECTRONIC SIGNATURE
+# Ben Chappell
+# Cole Kingery
+# Trevor Moorman
+# Alex Wiseman
+# 
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+# 
+# Handles low level communication and error handling with the Grove Ultrasonic Sensor. Mainly just a read function that takes the part and outputs the reading from the sensor.
+# Inputs: Surroundings
+# Outputs: Grove Ultrasonic Readings
