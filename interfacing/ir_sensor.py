e077f818741c27a17d7ed2df8b052a9444695014
Cole Kingery <kingerycole2@gmail.com>
2021-03-17 15:57:00 -0400

    Revert "Update ir_sensor.py"
    This reverts commit be2c0c248a790740dc48e8b64a0af2728bef2ebd.

--- interfacing/ir_sensor.py
+++ interfacing/ir_sensor.py
@@ -17,21 +17,14 @@
 # Cole Kingery
 # Trevor Moorman
 # Alex Wiseman
-#
+# 
 # The electronic signatures above indicate that the program
 # submitted for evaluation is the combined effort of all
 # team members and that each member of the team was an
 # equal participant in its creation. In addition, each
 # member of the team has a general understanding of
 # all aspects of the program development and execution.
-#
+# 
 # Low level communication and error handling with IR sensor.
 # Inputs: Surroundings
 # Outputs: IR Detection
-import time
-import grovepi
-
-def readIR(sensor): #sensor port is input for IR
-    grovepi.pinMode(sensor, "INPUT")
-
-    return(grovepi.analogRead(sensor))
