a01bfdca86dd9eabdb2bff341838351ba0bc7576
ImDaNoob <bjchappell02@icloud.com>
2021-03-17 14:06:37 -0400

    added imu interfacing code for magnet magnitude and acceleration values

--- interfacing/imu_interface.py
+++ interfacing/imu_interface.py
@@ -28,3 +28,46 @@
 # Low level communication and error handling with IMU
 # Inputs: Surroundings
 # Outputs: Magnetic Field Intensity, Velocity Data
+
+from MPU9250 import MPU9250
+import sys
+import time
+from math import sqrt
+
+# This gets all of the accel data and returns it as a dictionary with the major axes as keys.
+# Only argument takes the imu object.
+def getAccel(imu): 
+    # Get the acceleration reading from the IMU object
+    accel = imu.readAccel()
+
+    # Return the dictionary
+    return accel
+
+# Gets the x acceleration value given the mpu imu object.
+def getAccelX(imu):
+    # Call the main getAccel function to get the dictionary and return the x value
+    return getAccel(imu)['x']
+
+# Gets the y acceleration value given the mpu imu object.
+def getAccelY(imu):
+    # Call the main accel function to get the dictionary and return the y value
+    return getAccel(imu)['y']
+
+# Same as X and Y functions
+def getAccelZ(imu):
+    # Call main function
+    return getAccel(imu)['z']
+
+# Get all of the magnetic data.
+def getMagnet(imu):
+    # Get the magnetic reading from the imu object
+    magn = imu.readMagnet()
+
+    # Return the dictionary
+    return magn
+
+def getMagnMagnitude(imu):
+    # Calculate the magnitude of the magnetic force.
+    magnetMagnitude = sqrt((getMagnet(imu) ** 2) + (getMagnet(imu) ** 2) + (getMagnet(imu) ** 2))
+
+    return magnetMagnitude
