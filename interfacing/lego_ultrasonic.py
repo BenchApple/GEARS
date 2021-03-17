ee37efa6142cfc72f78aa132244fc4dcd86db097
Cole Kingery <kingerycole2@gmail.com>
2021-03-17 15:56:13 -0400

    Update lego_ultrasonic.py

--- interfacing/lego_ultrasonic.py
+++ interfacing/lego_ultrasonic.py
@@ -17,14 +17,25 @@
 # Cole Kingery
 # Trevor Moorman
 # Alex Wiseman
-# 
+#
 # The electronic signatures above indicate that the program
 # submitted for evaluation is the combined effort of all
 # team members and that each member of the team was an
 # equal participant in its creation. In addition, each
 # member of the team has a general understanding of
 # all aspects of the program development and execution.
-# 
+#
 # Handles low level communication and error handling with Lego Ultrasonic. Mainly just reading the sensor data.
 # Inputs: Surroundings
 # Outputs: Ultrasonic Readings
+
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division       #                           ''
+
+import time     # import the time library for the sleep function
+import brickpi3 # import the BrickPi3 drivers
+
+BP = brickpi3.BrickPi3()
+
+def legoUltrasonic(sensor): #port must be declared in same way as brick would i.e. BP.PORT_1
+    return(BP.get_sensor(sensor))
