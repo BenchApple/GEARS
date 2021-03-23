f529b38f9078532f43cd95e0de8a7ecde2b83252
tream2001 <45127883+tream2001@users.noreply.github.com>
2021-03-23 16:52:34 -0400

    Added functions to handle GEARS point turning.

--- driving/turning.py
+++ driving/turning.py
@@ -29,3 +29,45 @@
 # Turn the robot appropriately. Must be able to do proper 90 degree turns and keep the robot between the walls as it changes corridors.
 # Inputs: Driving, Wall Data, Maze Navigation Instructions
 # Outputs: Turns robot
+
+from GEARS.interfacing.motor import *
+ 
+# Description: Has the GEARS bot perform a point rotation centered on the middle of the wheel axis (the center of the GEARS bot)
+# Arguments: 
+#   bp = brickpi3 object
+#   left_motor_port = port # of the left motor (from the GEARS bot's perspective)
+#   right_motor_port = port # of the right motor (from the GEARS bot's perspective)
+#   dps = Speed at which the two motors will turn (will typically be set to the maximum dps defined in main)
+#   direction = "CW" for clockwise turning and "CCW" for counter-clockwise turning
+#   degrees = number of degrees the GEARS bot should turn
+def turn_X_degrees(bp, left_motor_port, right_motor_port, dps, direction, degrees):
+    set_dps(bp, LEFT_MOTOR_PORT, 0)
+    set_dps(bp, RIGHT_MOTOR_PORT, 0)
+    
+    degreesTurn = m.floor(((robotCircumference * (degrees / 360)) / wheelCircumference) * 360)
+
+    turnTime = (1 / dps) * degreesTurn
+
+    if (direction == "CW"):
+        set_dps(bp, LEFT_MOTOR_PORT, dps)
+        set_dps(bp, RIGHT_MOTOR_PORT, (-1 * dps))
+    elif (direction == "CCW"):
+        set_dps(bp, LEFT_MOTOR_PORT, (-1 * dps))
+        set_dps(bp, RIGHT_MOTOR_PORT, dps)
+
+    time.sleep(turnTime)
+
+    set_dps(bp, LEFT_MOTOR_PORT, 0)
+    set_dps(bp, RIGHT_MOTOR_PORT, 0)
+    
+    return
+  
+# Implementation of turn_X_degrees with the degrees given as 90 
+def turn_90_degrees(bp, left_motor_port, right_motor_port, dps, direction):
+    turn_X_degrees(bp, left_motor_port, right_motor_port, dps, direction, 90)
+    return
+
+# Implementation of turn_X_degrees with the degrees given as 180 and doesn't require a direction argument
+def turn_180_degrees(bp, left_motor_port, right_motor_port, dps):
+    turn_X_degrees(bp, left_motor_port, right_motor_port, dps, "CW", 180)
+    return
