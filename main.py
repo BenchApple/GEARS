3c71c63f34cb069f6b4bbbfa4797cf91814e4a20
Ben Chappell <bjchappell02@icloud.com>
2021-04-18 21:45:47 -0400

    finished standard intersectioning

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ from .hazards import hazard_sense
 from .maze import graph
 from .maze import maze_navigate as navigate
 from .maze import build_maze as build
+from .maze import maze_instructions as instruct
 from .misc import cargo_release as cargo
 from .walls import wall_sensing
 from . import constants as r
@@ -63,26 +64,10 @@ def main():
 
         # Now we check to see if we're backtracking and act accordingly.
         if not robot.is_backtracking :
-            # Compare the old node to the previous node to determine where to turn.
-            if (prev_node.get_orientation() + 1) % 4 == robot.cur_node.get_orientation():
-                # Turn 90 degrees clockwise.
-                turn.turn_90_degrees(robot.bp, robot.l_motor, robot.r_motor, robot.dps, "CW")
-
-                # Update the orientation of the robot.
-                robot.set_orientation(robot.cur_node.get_orientation() + 1)
-            
-            elif (prev_node.get_orientation() - 1) % 4 == robot.cur_node.get_orientation():
-                # Turn 90 degrees counterclockwise.
-                turn.turn_90_degrees(robot.bp, robot.l_motor, robot.r_motor, robot.dps, "CCW")
-                
-                # Update the orientation of the robot.
-                robot.set_orientation(robot.cur_node.get_orientation() - 1)
-            # This handles both a new node in front as well as a non-new node that's front.
-            else:
-                # Do nothing because nothing needs to be done. Orientation stays the same and we don't turn.
-                pass
-
-
+            instruct.standard_intersection(robot, prev_node)
+        else:
+            # Handle the backtracking here. This will suck, yes I know
+            pass
 
 if __name__ == "__main__":
     main()
--- maze/maze_instructions.py
+++ maze/maze_instructions.py
@@ -30,3 +30,29 @@
 # Most complex part will be backtracking and the simplest part will be forward navigation.
 # Inputs: Wall Data, Maze Navigation Decisions.
 # Outputs: Maze Instructions
+
+from ..driving import turning as turn
+from .. import constants as r
+
+# This function turns the robot based on where it needs to go
+# This takes the previous node and the robot object (assuming it's been updated to a new node)
+# compares the orientations, and turns apprpriately.
+def standard_intersection(robot, prev_node):
+    # Compare the old node to the previous node to determine where to turn.
+    if (prev_node.get_orientation() + 1) % 4 == robot.cur_node.get_orientation():
+        # Turn 90 degrees clockwise.
+        turn.turn_90_degrees(robot.bp, robot.l_motor, robot.r_motor, robot.dps, "CW")
+
+        # Update the orientation of the robot.
+        robot.set_orientation(robot.cur_node.get_orientation() + 1)
+    
+    elif (prev_node.get_orientation() - 1) % 4 == robot.cur_node.get_orientation():
+        # Turn 90 degrees counterclockwise.
+        turn.turn_90_degrees(robot.bp, robot.l_motor, robot.r_motor, robot.dps, "CCW")
+        
+        # Update the orientation of the robot.
+        robot.set_orientation(robot.cur_node.get_orientation() - 1)
+    # This handles both a new node in front as well as a non-new node that's front.
+    else:
+        # Do nothing because nothing needs to be done. Orientation stays the same and we don't turn.
+        pass
