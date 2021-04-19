362ae8a7a501785aa5df24dd2369f98501b3a5c3
Ben Chappell <bjchappell02@icloud.com>
2021-04-18 21:16:09 -0400

    started on main lop

---
+++ main.py
@@ -0,0 +1,40 @@
+# Project 3: GEARS Robot
+# File: main.py
+# Date: 3/14/21
+# By: Alex Wiseman
+# ajwisema
+# Ben Chappell
+# chappeb
+# Trevor Moorman
+# tmoorma
+# Cole Kingery
+# ckinger
+# Section: 5
+# Team: 70
+#
+# ELECTRONIC SIGNATURE
+# Ben Chappell
+# Cole Kingery
+# Trevor Moorman
+# Alex Wiseman
+# 
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+# 
+# This file holds the main loop of the robot. Imports everything, outputs a working robot.
+
+from .driving import propulsion as forward
+from .driving import turning as turn
+from .hazards import hazard_process
+from .hazards import hazard_sense
+
+# This is the main loop of the robot.
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
