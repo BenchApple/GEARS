3f6a731010097715beaf784b54415ec5b551d8b7
Ben Chappell <bjchappell02@icloud.com>
2021-04-26 22:35:47 -0400

    added output printing to file

--- hazards/hazard_output.py
+++ hazards/hazard_output.py
@@ -28,3 +28,13 @@
 # Outputs the location and type of each hazard.
 # Inputs: Hazard Location and Type
 # Outputs: Formal Hazard Output
+
+def output_hazards(robot):
+    output = open("GEARS/team70_hazards.csv")
+
+    output.write("Team: 70\nMap: 0\nNotes: This may have slight errors, and it has no actual parameter values.\n")
+    output.write("\nReseource Type, Parameter of Interest, Paramter, Resource X Coordinate, Resource Y Coordinate\n")
+
+    for hazard in robot.hazards_list:
+        output.write(str(hazard))
+        output.write("\n")
